46989fffbd7a46056e3776cecabcc30df2342ae6
george-593 <63100333+george-593@users.noreply.github.com>
2025-09-16 17:50:13 +0100

    Create configurations for multiple different games, and move config into a seperate file

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-import pyautogui, pytesseract, pyperclip, re
+import pyautogui, pytesseract, pyperclip, re, json
 from keyboard import is_pressed
 from time import sleep
 from os import getenv
 from dotenv import load_dotenv
 from openai import OpenAI
+
+import config
 
 # Dotenv
 load_dotenv()
@@ -16,6 +18,7 @@ client = OpenAI(api_key=getenv("OPENAI_API_KEY"))
 # Position of the text to capture
 # Four-integer tuple of the left, top, width, and height
 position = ()
+prompt = ""
 
 
 # Helper function to wait for a keypress
@@ -27,48 +30,27 @@ def waitForPress(key):
             sleep(0.1)
 
 
-# Get new positions if there is no saved pos
-def newPos():
-    # Get the positions from the user
-    print("Press F10 when your mouse is in the top-left corner of the text to capture")
-    waitForPress("F10")
-    topLeft = pyautogui.position()
-    sleep(0.1)
-
-    print(
-        "Press F10 when your mouse is in the bottom-right corner of the text to capture"
-    )
-    waitForPress("F10")
-    bottomRight = pyautogui.position()
-
-    # Calculate the width and height between the two points
-    width = bottomRight[0] - topLeft[0]
-    height = bottomRight[1] - topLeft[1]
-
-    # Save the new position data to data.txt
-    global position
-    position = (topLeft[0], topLeft[1], width, height)
-    with open("data/data.txt", "w") as f:
-        f.writelines([f"{str(num)}\n" for num in position])
-    print(f"Saved new position data: {position}")
-
-
-# Check if there is saved positions from data.txt
+# Load saved config data
 try:
-    with open("data/data.txt", "r") as f:
-        l = f.read()
-        if len(l) > 0:
-            try:
-                position = tuple([int(num) for num in l.splitlines()])
-            except ValueError:
-                print("Invalid data saved in data.txt")
-                newPos()
-            print(f"Retrieved saved position data: {position}")
-        else:
-            print("No saved data found in data.txt.")
-            newPos()
+    with open("data/config.json", "r") as f:
+        data = json.load(f)
+        gameNames = list(data["games"].keys())
+        selected = input(
+            f"Please select the game to load, Available options: {gameNames}\n"
+        )
+
+        try:
+            game = data["games"][selected]
+        except KeyError:
+            print("Invalid game name.")
+            exit()
+
+        position = (game["x"], game["y"], game["width"], game["height"])
+        print(position)
+        prompt = game["prompt"]
 except FileNotFoundError:
-    newPos()
+    print("No configuration found, entering config mode.")
+    config.decideConfig()
 
 # Main loop
 while True:
@@ -89,7 +71,7 @@ while True:
     print("Making ChatGPT Request")
     resp = client.responses.create(
         model="gpt-5-nano",
-        instructions="Answer with only the correct trivia answer. Keep answers as short as possible.",
+        instructions=prompt,
         input=txt,
     )
 
---
+++ config.py
@@ -0,0 +1,83 @@
+import pyautogui, keyboard, json
+from time import sleep
+
+
+def loadFile():
+    try:
+        # Try to open the file, will raise FileNotFoundError if the file doesn't exist
+        f = open("data/config.json", "r+")
+    except FileNotFoundError:
+        # Create the file
+        f = open("data/config.json", "w+")
+    return f
+
+
+def gameConfig():
+    # Print current game info
+    f = loadFile()
+    # Try to load JSON, if not possible create new base config
+    try:
+        data = json.load(f)
+        print(f"Current game data:\n{data['games']}")
+    except json.JSONDecodeError:
+        data = {"games": {}, "global": {}}
+        print("Unable to load current config")
+
+    # Get game related data
+    gameName = input("Enter the name of the game: ")
+    if gameName == "":
+        print("Game name was not entered.")
+        exit()
+
+    gamePrompt = input(
+        'Enter the prompt to be used for the game (Leave empty for: "Answer with only the correct trivia answer. Keep answers as short as possible.")'
+    )
+    if gamePrompt == "":
+        gamePrompt = "Answer with only the correct trivia answer. Keep answers as short as possible."
+
+    # Get the positions from the user
+    print("Press F10 when your mouse is in the top-left corner of the text to capture")
+    keyboard.wait("F10")
+    topLeft = pyautogui.position()
+
+    print(
+        "Press F10 when your mouse is in the bottom-right corner of the text to capture"
+    )
+    keyboard.wait("F10")
+    bottomRight = pyautogui.position()
+
+    # Calculate the width and height between the two points
+    width = bottomRight[0] - topLeft[0]
+    height = bottomRight[1] - topLeft[1]
+
+    data["games"][gameName] = {
+        "prompt": gamePrompt,
+        "x": topLeft[0],
+        "y": topLeft[1],
+        "width": width,
+        "height": height,
+    }
+    # Close the file and re-open in write mode so we can clear previous data and overwrite with new data
+    f.close()
+    f = open("data/config.json", "w")
+    json.dump(data, f)
+    f.close()
+
+
+def globalConfig():
+    pass
+
+
+def decideConfig():
+    choice = input(
+        "Would you like to configure global settings or game specific settings (global/games): "
+    )
+
+    if choice == "global":
+        globalConfig()
+    elif choice == "games":
+        gameConfig()
+
+
+if __name__ == "__main__":
+    decideConfig()
